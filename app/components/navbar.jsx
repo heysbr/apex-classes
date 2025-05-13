d355128d1b875616aeeccab1949e9508cd907f6a
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-13 15:50:00 +0530

    fix images src

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <div className='flex justify-between items-center h-[70px] bg-white px-[30px]'>
 
-        <div> <img className="h-12 my-auto rounded-xl " src="/images/apexlogo.svg" alt="Logo" /></div>
+        <div> <img className="h-12 my-auto rounded-xl " src="./images/apexlogo.svg" alt="Logo" /></div>
         <div className=' text-black'>Menu bar</div>
         <div>
             <button className='bg-red-600 rounded-xl px-6 py-1.5 font-bold'>Help Desk</button>
--- app/components/navtop.jsx
+++ app/components/navtop.jsx
@@ -5,10 +5,10 @@ export default function NavTop() {
   return (
     <div className=" flex justify-between bg-red-800 px-[60px] h-[40px] ">
       <div className="flex items-center gap-3.5">
-        <Image src="/images/facebook.svg" alt="Logo" width={30} height={30} />
-        <Image src="/images/twitter.svg" alt="Logo" width={30} height={30} />
-        <Image src="/images/instagram.svg" alt="Logo" width={30} height={30} />
-        <Image src="/images/linkedin.svg" alt="Logo" width={30} height={30} />
+        <Image src="./images/facebook.svg" alt="Logo" width={30} height={30} />
+        <Image src="./images/twitter.svg" alt="Logo" width={30} height={30} />
+        <Image src="./images/instagram.svg" alt="Logo" width={30} height={30} />
+        <Image src="./images/linkedin.svg" alt="Logo" width={30} height={30} />
         <a>hallo@gmail.com</a>
         <a>7895561007</a>
         <a>7060292022</a>
