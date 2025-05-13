57a9cb9ba534cff8a3bcf1067ae052622af7879b
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-13 15:42:47 +0530

    init

---
+++ app/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavTop from './navtop'
+import NavBar from './navbar'
+
+
+export default function Header() {
+  return (
+    <>
+    <NavTop/>
+    <NavBar/>
+    </>
+  )
+}
---
+++ app/components/navtop.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+export default function NavTop() {
+  return (
+    <div className=" flex justify-between bg-red-800 px-[60px] h-[40px] ">
+      <div className="flex items-center gap-3.5">
+        <Image src="/images/facebook.svg" alt="Logo" width={30} height={30} />
+        <Image src="/images/twitter.svg" alt="Logo" width={30} height={30} />
+        <Image src="/images/instagram.svg" alt="Logo" width={30} height={30} />
+        <Image src="/images/linkedin.svg" alt="Logo" width={30} height={30} />
+        <a>hallo@gmail.com</a>
+        <a>7895561007</a>
+        <a>7060292022</a>
+      </div>
+      <a className="mr-[60px] my-auto font-bold">Admin</a>
+    </div>
+  );
+}
