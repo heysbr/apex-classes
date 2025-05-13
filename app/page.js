c92f323913c059ce83d595312168bd8aad70d880
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-13 17:35:48 +0530

    Add Hero-section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Header from "./components/header";
+import HeroSection from "./components/hero-section";
 
 export default function Home() {
   return (
-<Header/>
+    <>
+      <Header />
+      <HeroSection />
+    </>
   );
 }
