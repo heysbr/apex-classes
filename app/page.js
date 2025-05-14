683dd5eef1553248a846cb513eb432909fe1eb5f
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-14 16:30:30 +0530

    Added Welcome

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
+import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
+      <WelcomeSection/>
     </>
   );
 }
