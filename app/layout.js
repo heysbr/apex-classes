e2a3aa18ee69eb66c27534ff6037da7277afe688
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-21 02:32:02 +0530

    test

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Roboto } from '@next/font/google';
+import { Roboto } from 'next/font/google';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
