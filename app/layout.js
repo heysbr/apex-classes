0759f111b4b395f67d043181850e5b8a7f343bdd
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-21 02:30:06 +0530

    test

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-
 import { Roboto } from '@next/font/google';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
