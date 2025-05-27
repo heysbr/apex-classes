4a08aa1b624534ca1b2cdd6fce34e2d89d49f632
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-27 11:39:52 +0530

    social logo fixed

--- app/components/Header/NavTop.jsx
+++ app/components/Header/NavTop.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
 
-import fb from "../../../public/images/facebook.svg"
-import twitter from "../../../public/images/twitter.svg"
-import linkedin from "../../../public/images/linkedin.svg"
+import fb from "../../../app/components/Assests/images/social/facebook.svg"
+import twitter from "../Assests/images/social/twitter.svg"
+import linkedin from "../Assests/images/social/linkedin.svg"
+
 
 
 export default function NavTop() {
@@ -21,9 +22,8 @@ export default function NavTop() {
   return (
     <div className=" flex justify-between bg-red-400 px-[60px] h-[40px] text-white">
       <div className="flex items-center gap-3.5">
-        {/* <Image src="./images/Teachers/fb.svg" alt="Logo" width={20} height={20} /> */}
          {socialLinks.map((item,i)=>(
-          <Image src={item.link} alt="Logo" width={20} key={i} height={20} />
+          <Image src={item.link} alt="Logo" width={30} key={i} height={20}/>
          ))}
         <div className=" hidden md:flex items-center gap-3">
         <a>hallo@gmail.com</a>
