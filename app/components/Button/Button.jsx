fc232c674a3f55cbea9cfb674d72b282408574ff
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-30 11:01:02 +0530

    Button Text Fixed

--- app/components/Button/Button.jsx
+++ app/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react'
 
 export default function Button({children}) {
   return (
-    <button className="hidden md:block bg-red-600  rounded-t-xl rounded-bl-xl px-6 py-1.5 font-bold">
+    <button className="hidden md:block bg-red-600  rounded-t-xl rounded-bl-xl px-6 py-1.5 font-bold text-white">
           {children}
         </button>
   )
