67caffbff83f9b240773cfc424c47dd0cbbc4d0c
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-05-30 13:50:39 +0530

    Added NavBarV2

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavTop from './NavTop'
 import NavBar from './NavBar'
+import NavBarV2 from './NavBarV2'
 
 
 export default function Header() {
   return (
     <div className='shadow-xl'>
     <NavTop/>
+    <NavBarV2/>
     <NavBar/>
     </div>
   )
